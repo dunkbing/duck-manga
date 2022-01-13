e544076308a9133b8afb034f4bb1f4353ef0ebf8
binh.buidang <dangbinh4869@gmail.com>
2022-01-13 23:30:35 +0700

    delete.

--- apps/api/src/entities/category.entity.ts
+++
@@ -1,9 +0,0 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-
-@Entity({ name: 'categories' })
-export class CategoryEntity {
-  @PrimaryGeneratedColumn()
-  id: number;
-  @Column('varchar', { length: 255 })
-  name: string;
-}
--- apps/api/src/entities/index.ts
+++ apps/api/src/entities/index.ts
@@ -1,5 +1,5 @@
 export * from './author.entity';
-export * from './category.entity';
+export * from './chapter-image.entity';
 export * from './chapter.entity';
 export * from './genre.entity';
 export * from './manga.entity';
