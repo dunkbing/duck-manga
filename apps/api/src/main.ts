967b32303dde7f4d5cff21993aba794b9bf958af
binh.buidang <dangbinh4869@gmail.com>
2022-01-15 00:23:19 +0700

    add: cors.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,9 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+  app.enableCors({ origin: process.env.CORS_ORIGIN });
+  const port = process.env.PORT;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
 
-bootstrap();
+void bootstrap();
