a0ba74e18fd64c9c6cf2345718ef8dd7a0d2e936
binh.buidang <dangbinh4869@gmail.com>
2022-01-18 14:37:41 +0700

    add: exception handler.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,7 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { GlobalExceptionFilter } from './common/exception-handler';
 import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
@@ -8,6 +9,7 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   app.enableCors({ origin: process.env.CORS_ORIGIN });
+  app.use(new GlobalExceptionFilter());
   const port = process.env.PORT;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
